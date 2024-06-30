24d5a762f4a9160b6c8324ac5cb2c9879dd3a214
Less <31947204+lessandsass@users.noreply.github.com>
2024-06-30 22:56:21 +0530

    forgot password feature
    input_type was added to forgot-password blade file.
    email has been sent with the status code.

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;
 
 class PasswordResetLinkController extends Controller
 {
     /**
      * Display the password reset link request view.
      */
-    public function create(): View
+    public function create()
     {
         return view('auth.forgot-password');
     }
@@ -25,20 +25,30 @@ class PasswordResetLinkController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+
+        $input_type = filter_var(
+            $request->input('input_type'), FILTER_VALIDATE_EMAIL
+            ) ? 'email' : 'useraname';
+            $request->merge([$input_type => $request->input('input_type')]);
+
         $request->validate([
-            'email' => ['required', 'email'],
+            'email' => ['required_without:username', 'email', 'exists:users,email'],
+            'username' => ['required_without:email', 'string', 'exists:users,username'],
         ]);
 
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
-            $request->only('email')
+            $request->only($input_type)
         );
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+        if ($status == Password::RESET_LINK_SENT) {
+            return back()->with('status', __($status));
+        }
+
+        throw ValidationException::withMessages([
+            $input_type => [trans($status)],
+        ]);
     }
 }
