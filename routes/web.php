e3b9e0f91a349417bbbac050aa8d12b67e72585a
Less <31947204+lessandsass@users.noreply.github.com>
2024-07-02 15:20:53 +0530

    Github and Google OAuth feature
    Initially github and google OAuth app created and GITHUB_CLIENT_ID,
    GITHUB_CLIENT_SECRET,
    GOOGLE_CLIENT_ID,
    GOOGLE_CLIENT_SECRET was generated.
    Attributes were created for GitHub and Google in the config/services.php file.
    redirect and callback routes were created.
    redirect and callback methods were created inside the ProviderController.
    Login buttons were added in the login blade file.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\ProviderController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/{provider}/redirect', [ProviderController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [ProviderController::class, 'callback']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
